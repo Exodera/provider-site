602448e3f0a760feeceb6db0010b8905e20ef6ba
Oratile Ditlhage <96663869+Exodera@users.noreply.github.com>
2022-11-06 10:53:09 +0200

    finished header and added banner

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {BiMenu} from 'react-icons/bi'
 import {Link} from 'react-router-dom'
 import './Header.css'
+import {BsCart} from 'react-icons/bs'
 
 const Header = () => {
   return (
@@ -19,15 +20,20 @@ const Header = () => {
         <div className='menu-links'>
             <Link to={'/'}>shop</Link>
             <Link to={'/'}>my swyft</Link>
-            <Link to={'/'}>activate sim</Link>
+            <Link to={'/'}>activate SIM</Link>
             <Link to={'/'}>support</Link>
-            <Link to={'/'}>sign in</Link>
-            <Link to={'/'}>cart</Link>
-
+            <p><Link className='sign-in' to={'/'}>sign in</Link></p>
+            <p>|</p>
+            <div className='cart'>
+            <Link className = 'cart-text' to={'/'}>cart</Link>
+            <Link to ={''}><BsCart className='cart-icon' size={'1.3rem'} color ="#9b9b9b"/></Link>
+            </div>
+           
+          
         </div>
         
         </div>
-        <hr color='#bbbcbc'/>
+        {/* <hr color='#bbbcbc'/> */}
     </div>
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,34 @@
 import React from 'react'
+import './styles/Home.css'
+import Nvidia from'../assets/nvidia.png'
+import Gaming from'../assets/gaming.jpg'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+        <div className='banner'>
+           <div className='banner-col1'>
+                <div className='banner-text'>
+                    <h2>swyftnet</h2>
+                    /
+                    <img src={Nvidia} alt= 'nvidia'/>
+                    <h2 className='nvidia'>nvidia</h2>
+                </div>
+
+                <p>swyftnet joins with NVIDIA to introduce GeForce NOW in South Africa</p>
+                <button>learn more</button>
+
+            </div >
+
+            <div className='banner-col2'>
+                <img src={Gaming} alt={'Gaming devices'}/>
+            </div>
+
+        </div>
+
+
+
+    </div>
   )
 }
 
